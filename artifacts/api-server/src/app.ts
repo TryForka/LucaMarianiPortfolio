46aec35880a4a89d7436ea3b06ee5e01a19e22ff
TryForka <forkabusiness@gmail.com>
2026-05-18 00:14:40 -0500

    Update1 app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -10,19 +10,19 @@ app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
-        return {
-          id: req.id,
-          method: req.method,
-          url: req.url?.split("?")[0],
+  req(req: any) {
+    return {
+      id: req.id,
+      method: req.method,
+      url: req.url?.split("?")[0],
+    };
+  },
+  res(res: any) {
+    return {
+      statusCode: res.statusCode,
         };
       },
-      res(res) {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
-    },
+     },
   }),
 );
 app.use(cors());
